2bb79c239bff1f131fa4ea8f55a397a9e39a838a
pawelgorski <40346731+pawelgorski@users.noreply.github.com>
2023-12-16 19:02:11 +0100

    add image link response (#46)

---
+++ backend/src/main/java/com/rcbsukcesja/hack2react/model/dto/view/ImageLinkView.java
@@ -0,0 +1,16 @@
+package com.rcbsukcesja.hack2react.model.dto.view;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+public class ImageLinkView {
+    private String imageLink;
+}
