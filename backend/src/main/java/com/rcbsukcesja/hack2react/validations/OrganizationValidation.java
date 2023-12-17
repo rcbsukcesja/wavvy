bd3ca22b2f79a5b9e073b26ee9cfcedc5ef839fb
pawelgorski <40346731+pawelgorski@users.noreply.github.com>
2023-12-17 11:54:05 +0100

    organization validation during acceptance (#47)

--- backend/src/main/java/com/rcbsukcesja/hack2react/validations/OrganizationValidation.java
+++ backend/src/main/java/com/rcbsukcesja/hack2react/validations/OrganizationValidation.java
@@ -1,6 +1,9 @@
 package com.rcbsukcesja.hack2react.validations;
 
 import com.rcbsukcesja.hack2react.exceptions.alreadyExists.OrganizationAlreadyExistsException;
+import com.rcbsukcesja.hack2react.exceptions.badrequest.InvalidKrsException;
+import com.rcbsukcesja.hack2react.exceptions.badrequest.InvalidNipException;
+import com.rcbsukcesja.hack2react.exceptions.badrequest.InvalidRegonException;
 import com.rcbsukcesja.hack2react.exceptions.forbidden.ForbiddenAccessDeniedException;
 import com.rcbsukcesja.hack2react.exceptions.messages.ErrorMessages;
 import com.rcbsukcesja.hack2react.model.entity.Organization;
@@ -21,7 +24,7 @@ public class OrganizationValidation {
     }
 
     public void checkIfOrganizationKrsAlreadyExists(String krs) {
-        if (krs == null) return;
+        if (krs == null || krs.isBlank()) return;
         if (organizationRepository.existsByKrs(krs)) {
             throw new OrganizationAlreadyExistsException(
                     ErrorMessages.ORGANIZATION_KRS_ALREADY_EXISTS, krs);
@@ -29,7 +32,7 @@ public class OrganizationValidation {
     }
 
     public void checkIfOrganizationNipAlreadyExists(String nip) {
-        if (nip == null) return;
+        if (nip == null || nip.isBlank()) return;
         if (organizationRepository.existsByNip(nip)) {
             throw new OrganizationAlreadyExistsException(
                     ErrorMessages.ORGANIZATION_NIP_ALREADY_EXISTS, nip);
@@ -37,7 +40,7 @@ public class OrganizationValidation {
     }
 
     public void checkIfOrganizationRegonAlreadyExists(String regon) {
-        if (regon == null) return;
+        if (regon == null || regon.isBlank()) return;
         if (organizationRepository.existsByRegon(regon)) {
             throw new OrganizationAlreadyExistsException(
                     ErrorMessages.ORGANIZATION_REGON_ALREADY_EXISTS, regon);
@@ -48,5 +51,52 @@ public class OrganizationValidation {
         if (organization.isDisabled() || !organization.isConfirmed()) {
             throw new ForbiddenAccessDeniedException(ErrorMessages.ACCESS_DENIED_MESSAGE);
         }
+    }
+
+    public void checkIfOrganizationNameAlreadyExistsWhileConfirming(String name) {
+        if (organizationRepository.findAllByNameIgnoreCase(name).size() > 1) {
+            throw new OrganizationAlreadyExistsException(
+                    ErrorMessages.ORGANIZATION_NAME_ALREADY_EXISTS, name);
+        }
+    }
+
+    public void checkIfOrganizationKrsAlreadyExistsWhileConfirming(String krs) {
+        if (krs == null || krs.isBlank()) return;
+        if (!ValidationConstants.patternNip.matcher(krs).matches()) {
+            throw new InvalidKrsException(ErrorMessages.KRS_VALIDATION_ERROR);
+        }
+        if (organizationRepository.findAllByKrs(krs).size() > 1) {
+            throw new OrganizationAlreadyExistsException(
+                    ErrorMessages.ORGANIZATION_KRS_ALREADY_EXISTS, krs);
+        }
+    }
+
+    public void checkIfOrganizationNipAlreadyExistsWhileConfirming(String nip) {
+        if (nip == null || nip.isBlank()) return;
+        if (!ValidationConstants.patternNip.matcher(nip).matches()) {
+            throw new InvalidNipException(ErrorMessages.NIP_VALIDATION_ERROR);
+        }
+        if (organizationRepository.findAllByNip(nip).size() > 1) {
+            throw new OrganizationAlreadyExistsException(
+                    ErrorMessages.ORGANIZATION_NIP_ALREADY_EXISTS, nip);
+        }
+    }
+
+    public void checkIfOrganizationRegonAlreadyExistsWhileConfirming(String regon) {
+        if (regon == null || regon.isBlank()) return;
+        if (!ValidationConstants.patternRegon.matcher(regon).matches()) {
+            throw new InvalidRegonException(ErrorMessages.REGON_VALIDATION_ERROR);
+        }
+        if (organizationRepository.findAllByRegon(regon).size() > 1) {
+            throw new OrganizationAlreadyExistsException(
+                    ErrorMessages.ORGANIZATION_REGON_ALREADY_EXISTS, regon);
+        }
+    }
+
+    public void validateOfficialOrganizationFieldsDuringConfirmation(Organization organization) {
+        checkIfOrganizationNameAlreadyExistsWhileConfirming(organization.getName());
+        checkIfOrganizationNipAlreadyExistsWhileConfirming(organization.getNip());
+        checkIfOrganizationKrsAlreadyExistsWhileConfirming(organization.getKrs());
+        checkIfOrganizationRegonAlreadyExistsWhileConfirming(organization.getRegon());
     }
 }
