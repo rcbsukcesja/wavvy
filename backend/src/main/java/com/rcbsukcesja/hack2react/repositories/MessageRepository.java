9d5baf170e605614001288203256c2dae781bacd
pawelgorski <40346731+pawelgorski@users.noreply.github.com>
2023-12-17 21:18:10 +0100

    message pagination and search (#48)

--- backend/src/main/java/com/rcbsukcesja/hack2react/repositories/MessageRepository.java
+++ backend/src/main/java/com/rcbsukcesja/hack2react/repositories/MessageRepository.java
@@ -1,19 +1,23 @@
 package com.rcbsukcesja.hack2react.repositories;
 
 import com.rcbsukcesja.hack2react.model.entity.Message;
-import com.rcbsukcesja.hack2react.model.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface MessageRepository extends JpaRepository<Message, UUID> {
+public interface MessageRepository extends JpaRepository<Message, UUID>, JpaSpecificationExecutor<Message> {
     Optional<Message> getMessageById(UUID id);
 
-    List<Message> getMessageBySender(User sender);
+    @NonNull
+    Page<Message> findAll(@Nullable Specification<Message> spec, @Nullable Pageable pageable);
 
-    List<Message> getMessageByReceiver(User receiver);
 }
